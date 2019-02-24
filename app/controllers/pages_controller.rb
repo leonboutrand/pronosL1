7b5710cb7eb5f00bae30bbacad4b8867b275a082
Leon Boutrand <leonboutrand@gmail.com>
2019-02-24 22:04:36 +0100

    live working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,29 +3,71 @@ require 'open-uri'
 class PagesController < ApplicationController
   def home
     @users = User.all
-    @scores = scraper(26).join("\n")
+  end
+
+  def live
+    @scores = scraper(26, true)
+  end
+
+  def pronos
+    @scores = scraper(26, false)
   end
 
   private
 
-  def scraper(journee)
+  def scraper(journee, live)
     url = "https://www.lfp.fr/ligue1/calendrier_resultat?sai=102&jour=#{journee}"
     html_content = open(url).read
     doc = Nokogiri::HTML(html_content)
+    scores = scrap_scores(doc) if live
+    home = scrap_home(doc)
+    away = scrap_away(doc)
+    games = []
+    if live
+      scores.each_with_index do |score, i|
+        games << { home: home[i], score_home: score[0], score_away: score[1], away: away[i] }
+      end
+    else
+      home.each_with_index do |_team, i|
+        games << { home: home[i], away: away[i] }
+      end
+    end
+    games
+  end
+
+  def scrap_scores(doc)
     results = []
     doc.search('.stats a').each do |score|
       if score.text.strip != ''
-        results << score.text.strip
-      elsif score.attributes['href'].value =~ /feuille/
+        results << score.text.strip.split(' - ').map { |x| x.to_i }
+      elsif score.attributes['href'].value.match?(/feuille/)
         results << scrap_inside(score.attributes['href'].value)
+      else
+        results << 'Pas commencé'
       end
     end
-    p results
+    results
   end
 
   def scrap_inside(path)
     url = "https://www.lfp.fr/#{path}"
     inside = Nokogiri::HTML(open(url).read)
-    inside.search('.buts').map { |x| x.text.strip }.join(' - ')
+    inside.search('.buts').map { |x| x.text.strip.to_i }
+  end
+
+  def scrap_home(doc)
+    results = []
+    doc.search('.domicile a').each do |team|
+      results << team.text.strip
+    end
+    results
+  end
+
+  def scrap_away(doc)
+    results = []
+    doc.search('.exterieur a').each do |team|
+      results << team.text.strip
+    end
+    results
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :articles
   devise_for :users
+
+  get 'live', to: 'pages#live', as: :live
+  get 'pronos', to: 'pages#pronos', as: :pronos
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
