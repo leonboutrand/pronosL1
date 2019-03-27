27c6ad01d9164769ff0f18e6d54fe37d31b69dad
Leon <leonboutrand@gmail.com>
2019-03-27 14:49:37 +0100

    js ok to submit all bets in 1 click, still need refacto

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -1,4 +1,6 @@
 class BetsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create]
+
   def create
     @bet = Bet.new(bet_params)
     @bet.user = current_user
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,3 +2,12 @@ import "bootstrap";
 import commentsScroller from './commentsScroller.js'
 
 commentsScroller();
+
+document.querySelector('#submit-bets').addEventListener('click', (event) => {
+  console.log('yes1')
+  document.querySelectorAll('.new_bet .bet-submit').forEach((submitBtn) => {
+  console.log('yes2')
+    submitBtn.click();
+  })
+}
+)
