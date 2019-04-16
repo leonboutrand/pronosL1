9fdb962801a6044c7fd146610d2c1073f62b3dea
Leon <leonboutrand@gmail.com>
2019-04-16 13:40:20 +0200

    test 1

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,14 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should be 9 users in db" do
+    User.create! (
+      {
+      pseudo: 'test',
+      email: "test@yes.com",
+      password: "123456",
+      }
+    )
+    assert User.all.count == 1
+  end
 end
