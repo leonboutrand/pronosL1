7f34746d2af2325d2285f84db7713d1eac61df35
Leon <leonboutrand@gmail.com>
2019-05-15 23:24:03 +0200

    matchday updated

--- app/models/game.rb
+++ app/models/game.rb
@@ -6,7 +6,7 @@ class Game < ApplicationRecord
   # validates :status, inclusion: { in: %w[pending open live finished] }
 
   def self.current_matchday
-    36
+    37
   end
 
   def self.opened_matchdays
